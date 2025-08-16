3d5fa98e12246bfcc47080bb2b5da8c82b6960d9
DevMatheusGama <devmatheusgama@gmail.com>
2025-08-16 08:48:51 -0300

    Criando estrura e componentes da list

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import AddList from './components/AddList/AddList'
+import List from './components/List/List'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='Container'>
+      <AddList />
+      <List />
+    </div>
   )
 }
 
---
+++ to-do-list/src/components/AddList/AddList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../button/Button'
+
+function AddList() {
+    return (
+        <div>
+            <input type="text" />
+            <Button />
+        </div>
+    )
+}
+
+export default AddList
