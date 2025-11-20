2f1d3ab7c7cc77918d3d5b7b403d26859e4d9b66
DevMatheusGama <devmatheusgama@gmail.com>
2025-11-20 16:55:07 -0300

    ajustes finais prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,13 @@
-export default {
-    datasource: {
-        url: process.env.DATABASE_URL,
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
+    schema: 'prisma/schema.prisma',
+    migrations: {
+        path: 'prisma/migrations',
+        seed: `tsx prisma/seed.ts`,
     },
-};
+    datasource: {
+        url: env('DATABASE_URL'),
+    },
+});
